859e06a142fb9cceebf0b2e4da93f11b2f4b385a
graek <lukas@graessle.org>
2018-05-24 11:24:05 +0200

    added FilePond for file-upload

--- src/components/FileUpload/FileUpload.js
+++ src/components/FileUpload/FileUpload.js
@@ -1,8 +1,23 @@
 import React, { Component } from 'react';
 
-import FineUploaderTraditional from 'fine-uploader-wrappers';
-import Gallery from 'react-fine-uploader';
+//import FineUploaderTraditional from 'fine-uploader-wrappers';
+//import Gallery from 'react-fine-uploader';
+//import 'react-fine-uploader/gallery/gallery.css'
 
+
+
+// Import React FilePond
+import { FilePond, File, registerPlugin } from 'react-filepond';
+
+// Import FilePond styles
+import 'filepond/dist/filepond.min.css';
+
+// Register the image preview plugin
+//import FilePondImagePreview from 'filepond-plugin-image-preview';
+//import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css';
+//registerPlugin(FilePondImagePreview);
+
+/*
 const uploader = new FineUploaderTraditional({
     options: {
         chunking: {
@@ -19,13 +34,24 @@ const uploader = new FineUploaderTraditional({
             enableAuto: true
         }
     }
-});
+})
+*/
 
 class FileUpload extends Component {
     render() {
         return (
-            <Gallery uploader={ uploader } />
-        )
+            //<div></div>
+            //<Filepond allowMultiple={true} maxFiles={3} server="/api"/>
+            <div>
+
+                {/* Pass FilePond properties as attributes */}
+                <FilePond allowMultiple={true} maxFiles={3} server="/api">
+
+
+
+                </FilePond>
+            </div>
+        );
     }
 }
 
